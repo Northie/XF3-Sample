49ed5a1ae0643c958ec5b72722f65a17ea635088
michaelcockle <michael.cockle@gohomedirect.com>
2018-10-08 15:16:33 +0100

    refactor app structure

--- server/settings/database.trait.php
+++ server/settings/database.trait.php
@@ -11,11 +11,11 @@ trait _database {
             $settings['app_cache']['type'] = 'file';
             $settings['app_cache']['path'] =  X1_DAT_PATH.'/cache/';
             
-            $settings['app_cache']['type'] = 'couchbase';
-            $settings['app_cache']['host'] = '127.0.0.1';
-            $settings['app_cache']['user'] = '';
-            $settings['app_cache']['pass'] = 'cachepass';
-            $settings['app_cache']['name'] = 'cache';
+            $settings['app_cache']['type'] = 'none';
+//            $settings['app_cache']['host'] = '127.0.0.1';
+//            $settings['app_cache']['user'] = '';
+//            $settings['app_cache']['pass'] = 'cachepass';
+//            $settings['app_cache']['name'] = 'cache';
             
             $settings['data']['type'] = '';
             $settings['data']['host'] = '';
